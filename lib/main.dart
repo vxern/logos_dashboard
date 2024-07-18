86014a4c85ab90a14e64f05465f8a355252ec30f
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-07-18 21:20:30 +0100

    feat: Add `go_router`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() => runApp(const LogosDashboard());
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const LogosDashboard(),
+    ),
+  ],
+);
 
 class LogosDashboard extends StatelessWidget {
   const LogosDashboard({super.key});
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
+  Widget build(BuildContext context) => MaterialApp.router(
       title: 'Logos - Dashboard',
-      home: Scaffold(),
+      routerConfig: _router,
     );
 }
 

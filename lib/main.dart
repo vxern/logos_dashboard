71a58cab1d14d7f20f2eb2774fc8b489b3a13091
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-07-20 13:40:19 +0100

    feat: Add localisation support.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,32 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:go_router/go_router.dart';
 import 'package:logos_dashboard/pages/home.dart';
 
-void main() {
+void main() async {
   setUrlStrategy(PathUrlStrategy());
 
-  runApp(const LogosDashboard());
+  WidgetsFlutterBinding.ensureInitialized();
+  EasyLocalization.logger.enableBuildModes = const [];
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('en', 'GB'),
+      ],
+      fallbackLocale: const Locale('en', 'GB'),
+      path: 'assets/localisations',
+      child: const ScreenUtilInit(
+        designSize: Size(800, 600),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        child: LogosDashboard(),
+      ),
+    ),
+  );
 }
 
 final _router = GoRouter(
@@ -24,19 +43,20 @@ class LogosDashboard extends StatelessWidget {
   const LogosDashboard({super.key});
 
   @override
-  Widget build(BuildContext context) => ScreenUtilInit(
-        designSize: const Size(800, 600),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        child: MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          title: 'Logos - Dashboard',
-          routerConfig: _router,
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.lightBlue,
-              brightness: Brightness.dark,
-            ),
+  Widget build(BuildContext context) => MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        // We do not localise here because we're at the root of where
+        // localisation is provided to the context, meaning `context.tr()` will
+        // not work.
+        title: 'Logos Dashboard',
+        routerConfig: _router,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.lightBlue,
+            brightness: Brightness.dark,
           ),
         ),
       );

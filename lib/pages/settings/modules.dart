18beea542242c5d803261995d463369a43d46e1b
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-07-21 21:03:06 +0100

    misc: Update URI of import.

--- lib/pages/settings/modules.dart
+++ lib/pages/settings/modules.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:logos_dashboard/structs/module.dart';
-import 'package:logos_dashboard/widgets/module_card.dart';
+import 'package:logos_dashboard/widgets/cards/module_card.dart';
 
 class ModulesPage extends StatelessWidget {
   const ModulesPage();

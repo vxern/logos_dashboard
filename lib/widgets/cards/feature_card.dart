a2c546f3e77bddd83ff9bb1f0bb8fc88b9d373ba
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-07-27 23:02:30 +0100

    feat: Update feature card.

--- lib/widgets/cards/feature_card.dart
+++ lib/widgets/cards/feature_card.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:logos_dashboard/structs/feature.dart';
 import 'package:logos_dashboard/widgets/logos_card.dart';
+import 'package:logos_dashboard/widgets/logos_field.dart';
 
 class FeatureCard extends LogosCard {
   final GlobalKey _formKey;
+  final Feature feature;
 
   FeatureCard._({
     required super.title,
     required super.description,
     required super.icon,
     required super.color,
+    required this.feature,
   }) : _formKey = GlobalKey<FormBuilderState>();
 
   factory FeatureCard.fromFeature(
@@ -19,17 +22,21 @@ class FeatureCard extends LogosCard {
     Feature feature,
   ) =>
       FeatureCard._(
-        title: context.tr('features.${feature.name}.title'),
-        description: context.tr('features.${feature.name}.description'),
+        title: context.tr(feature.title),
+        description: context.tr(feature.description),
         color: Theme.of(context).primaryColorLight,
         icon: feature.icon,
+        feature: feature,
       );
 
   @override
   Widget body(BuildContext context) => FormBuilder(
         key: _formKey,
-        child: Column(
-          children: [FormBuilderTextField(name: 'a')],
-        ),
+        child: Column(children: _featureFields()),
       );
+
+  List<Widget> _featureFields() => feature.options
+      .map(LogosField.fromOption)
+      .map((field) => Expanded(child: field))
+      .toList();
 }
